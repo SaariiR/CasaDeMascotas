c5b374e76c85a2632ccf0a6cc929e5487476b310
SaariiR <saarii.rodriguez@gmail.com>
2016-05-24 14:33:32 -0600

    Contoller de login

---
+++ app/controllers/Ingresar.java
@@ -0,0 +1,16 @@
+package controllers;
+import models.Login;
+import play.*;
+import play.db.jpa.Model;
+import play.mvc.*;
+
+
+/**
+ *
+ * @author Rebeca Sarahi Guzman Robles 244645
+ * @author Sara Rodriguez Soto 262002
+ *
+ */
+public class Ingresar extends Model{
+    
+}
