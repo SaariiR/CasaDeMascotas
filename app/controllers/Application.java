a388066212ca9ed7ef06b60e12f9856caca89cf1
SaariiR <saarii.rodriguez@gmail.com>
2016-05-24 14:16:27 -0600

    Configuracion  para el llenado de los formularios

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,64 +7,75 @@ import models.*;
 import play.data.validation.*;
 import play.mvc.*;
 
-
 /**
  *
  * @author Rebeca Sarahi Guzman Robles 244645
  * @author Sara Rodriguez Soto 262002
  *
  */
-
 public class Application extends Controller {
 
     public static void index() {
         render();
     }
-   
-    public static void apadrina(){
-     render();
-    }
-  
-    public static void contactar(){
-     render();
-    }
-  
-    public static void hazteSocio(String nombre, String apellidos, String fechaDeNacimiento,
-            String usuario, String clave, String email, int telefono, String titular, 
-            int cuenta, String periocidad, int cantidad, String comentarios){
-            
-        AltaSocio socio = new AltaSocio(nombre, apellidos, fechaDeNacimiento, usuario, clave,
-                      email,  telefono,  titular,  cuenta, periocidad,  cantidad, comentarios);
-        socio.save();
-      
+
+    public static void apadrina(String nombreM, String nombre, String apellidos,
+            String fechaDeNacimiento, String email, int telefono, String titular, 
+            int cuenta, String periocidad, int cantidad, String comentarios) {
+
+        Apadrina padrino = new Apadrina(nombreM, nombre, apellidos, fechaDeNacimiento, 
+                email, telefono,titular, cuenta, periocidad, cantidad, comentarios);
+        padrino.save();
+
         render();
     }
-  
-    public static void loginMas(){
-     render();
-    }
-  
-    public static void ver(){
-     render();
-    }
-  
-    public static void verMas(){
-     render();
-    }
-    
-     public static void getGatos() {
+
+    public static void contactar(String nombreApe, String email,int telefono,
+            String localidad , String asunto , String mensaje ) {
         
+        Contactar contacto = new Contactar (nombreApe, email,telefono,
+            localidad , asunto, mensaje);
+        contacto.save();
+        
+        render();
+    }
+
+    public static void hazteSocio(String nombre, String apellidos, String fechaDeNacimiento,
+            String usuario, String clave, String email, int telefono, String titular,
+            int cuenta, String periocidad, int cantidad, String comentarios) {
+
+        AltaSocio socio = new AltaSocio(nombre, apellidos, fechaDeNacimiento, usuario, clave,
+                email, telefono, titular, cuenta, periocidad, cantidad, comentarios);
+        socio.save();
+
+        render();
+    }
+
+    public static void loginMas() {
+        render();
+    }
+
+    public static void ver() {
+        render();
+    }
+
+    public static void verMas() {
+        render();
+    }
+
+    public static void getGatos() {
+
         List<MascotaGatos> lista = MascotaGatos.findAll();
-        
+
         renderJSON(
-                    new JSONSerializer()
-                            .include("id", "nombre", "descripcion", "localidad", "imagen")
-                            .exclude("*")
-                            .serialize(lista)
-                  );   
-       
+                new JSONSerializer()
+                .include("id", "nombre", "descripcion", "localidad", "imagen")
+                .exclude("*")
+                .serialize(lista)
+        );
+
     }
-  
- 
+
     
+
 }
